5191b01cc82a927fcaebc83052f0388f05cc8006
timo334 <timothyodek895@gmail.com>
2025-01-03 10:48:50 +0300

    Delete ETHEREUM_FRAUD_DETECTION-main directory

--- ETHEREUM_FRAUD_DETECTION-main/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-input_addr=input("Input the reciever\'s Ethereum Account Address : ")
-
-
-if input_addr in fraudulent_dataset:
-    print("The model predicts that the transaction associated with the provided address is a fraud account.")
-else:
-    print("The model predicts that the transaction associated with the provided address is a non-fraud account.")
